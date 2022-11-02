c0d4807245cd47c2963dd4f07de40696a19499ac
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-02 13:04:07 +0530

    methodbased

---
+++ CSharpMoreConcepts/LINQ/MethodOriented.cs
@@ -0,0 +1,30 @@
+﻿using CSharpMoreConcepts.Data;
+using CSharpMoreConcepts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpMoreConcepts.LINQ
+{
+    internal class MethodOriented
+    {
+        static void Main(string[] n) {
+            var studentdata = StudentData.list;
+            var result = studentdata.Where(x => x.Percentage > 80).OrderByDescending(x=>x.Percentage);
+
+            foreach (var x in result)
+                Console.WriteLine(x);
+
+            var avg = studentdata.Average(x=>x.Percentage);
+
+            Console.WriteLine("Average of all the students : {0}", avg);
+
+            var cities = studentdata.GroupBy(s => s.City);
+
+            foreach (var x in cities) {
+                Console.WriteLine(" {0} -> Average : {1}", x.Key, x.Average(s => s.Percentage));
+            }
+        }
+    }
+}
