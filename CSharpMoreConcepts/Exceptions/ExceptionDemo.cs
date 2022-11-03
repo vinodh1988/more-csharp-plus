9997b5cc5f6feddddafb9983ee31bde5cc8f33e9
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-03 10:31:17 +0530

    own-exceptions

--- CSharpMoreConcepts/Exceptions/ExceptionDemo.cs
+++ CSharpMoreConcepts/Exceptions/ExceptionDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSharpMoreConcepts.Exceptions
@@ -7,6 +8,7 @@ namespace CSharpMoreConcepts.Exceptions
     internal class ExceptionDemo
     {
         static void Main(string[] n) {
+            int[] existingIds = { 343, 43, 435, 35, 346, 32, 22, 2, 25, 2 };
             try
             {
                 Console.WriteLine("Enter an integer");
@@ -15,17 +17,33 @@ namespace CSharpMoreConcepts.Exceptions
                 int b = Int32.Parse(Console.ReadLine());
                 int c = a / b;
                 Console.WriteLine("a/b= {0}", a / b);
+                Console.WriteLine("Enter a number for the id");
+                int id = Int32.Parse(Console.ReadLine());
+                if (existingIds.Contains(id))
+                    throw new RecordAlreadyExistsException();
+                else
+                    Console.WriteLine("Creating a new Record with id" + id);
             }
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Denominator should not be zero");
             }
-            catch (FormatException) {
+            catch (FormatException)
+            {
                 Console.WriteLine("The value Which you entered is not in numberic format");
+            }
+            catch (RecordAlreadyExistsException e)
+            {
+                Console.WriteLine(e.Message + " " + e.StackTrace);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message + " " + e.StackTrace + " " + e.GetType());
+            }
+            finally
+            {
+
+                Console.WriteLine("Code Always Runs");
             }
         }
         /*
---
+++ CSharpMoreConcepts/Exceptions/RecordALreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMoreConcepts.Exceptions
+{
+    internal class RecordAlreadyExistsException: Exception
+    {
+       new public string Message = "Record already Exists";
+        public override string ToString() {
+            return Message+ " with that id";
+        }
+    }
+}
