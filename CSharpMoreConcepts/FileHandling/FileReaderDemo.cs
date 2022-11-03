61f4c8d6f978ca06ef21966b72fb7bc7fbe319aa
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-03 12:53:42 +0530

    stream-reader

---
+++ CSharpMoreConcepts/FileHandling/FileReaderDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.FileHandling
+{
+    internal class FileReaderDemo
+    {
+        static void Main(String[] n) {
+            StreamReader sr = new StreamReader("d:\\myfiles\\sample.txt");
+            String str = null;
+            while ((str=sr.ReadLine())!=null) { 
+               Console.WriteLine(str);
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ CSharpMoreConcepts/FileHandling/FileWrite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.FileHandling
+{
+    internal class FileWrite
+    {
+        static void Main(string[] n) {
+            StreamWriter stream = new StreamWriter("D:\\myfiles\\sample.txt");
+            string sample = "";
+            while (!sample.Equals("End")) { 
+              sample = Console.ReadLine();
+                stream.WriteLine(sample);
+            }
+            stream.Flush();
+            stream.Close();
+        }
+    }
+}
