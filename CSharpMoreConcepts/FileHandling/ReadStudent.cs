031dc76daef70ea6b077c92239ad480b5611929e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-04 10:12:07 +0530

    store-txt

---
+++ CSharpMoreConcepts/FileHandling/ReadStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.FileHandling
+{
+    internal class ReadStudent
+    {
+        static void Main(String[] n) {
+            try
+            {
+                StreamReader str = new StreamReader("d:\\myfiles\\studentdata.txt");
+                string x = null;
+                while ((x = str.ReadLine()) != null)
+                {
+                    string[] array = x.Split(" ");
+                    foreach (var p in array)
+                        if(!p.Equals(""))
+                        Console.WriteLine(p);
+                }
+            }
+            catch (FileNotFoundException e) {
+                Console.WriteLine("File not exists");
+            }
+        }
+    }
+}
---
+++ CSharpMoreConcepts/FileHandling/WriteStudent.cs
@@ -0,0 +1,37 @@
+﻿using CSharpMoreConcepts.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.FileHandling
+{
+    internal class WriteStudent
+    {
+        static void Main(string[] n) {
+            StreamWriter sw = new StreamWriter("D:\\myfiles\\studentdata.txt");
+            while (true)
+            {
+                Console.WriteLine("Enter Sno:");
+                int sno=Int32.Parse(Console.ReadLine());
+                if (sno <= 0)
+                    break;
+                Console.WriteLine("Enter Name:");
+                String name = Console.ReadLine();
+                Console.WriteLine("Enter City:");
+                String city = Console.ReadLine();
+               /* Student student = new Student()
+                {
+                    Sno = sno,
+                    Name = name,
+                    City =city
+                };*/
+                String data = String.Format("{0,10}{1,20}{2,20}",sno,name,city);
+                sw.WriteLine(data);
+               
+            }
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
