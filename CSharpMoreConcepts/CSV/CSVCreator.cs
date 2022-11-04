2b6215a26d2cfffd2b5cf947bc2206f5f4435e7a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-04 11:48:58 +0530

    csv-files

---
+++ CSharpMoreConcepts/CSV/CSVCreator.cs
@@ -0,0 +1,22 @@
+﻿using CSharpMoreConcepts.Data;
+using CSharpMoreConcepts.Entities;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.CSV
+{
+    internal class CSVCreator
+    {
+        static void Main(string[] s) {
+            List<Student> list=StudentData.list;
+            using (var writer = new StreamWriter("D:\\myfiles\\student.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture)) { 
+               csv.WriteRecords(list);
+            }
+        }
+    }
+}
---
+++ CSharpMoreConcepts/CSV/CSVReader.cs
@@ -0,0 +1,24 @@
+﻿using CSharpMoreConcepts.Entities;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CSharpMoreConcepts.CSV
+{
+    internal class CSVReader
+    {
+
+        static void Main(string[] str) {
+            using (var reader = new StreamReader("d:\\myfiles\\student.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Student>();
+                foreach (var x in records)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+}
